632c90116940da42e000163fbdc8551b81478d10
史孝开 <shixiaokai@chinamobile.com>
2021-06-02 15:33:40 +0800

    docs(网络排查命令): 网络命令ss、nc
    ss与netstat差不多，显示所有socket相关信息，nc可以模拟TCP/UDP协议，监听端口

---
+++ README.md
@@ -0,0 +1,4 @@
+# atme-linux
+
+## [linux-常用shell命令](linux-常用shell命令.md)
+## [linux-常用排查问题命令](linux-常用排查问题命令.md)
---
+++ src/main/resources/shell/alias.sh
@@ -0,0 +1,3 @@
+alias cdsh='cd /opt/atme-linux/src/main/resources'
+alias sshdb='ssh root@10.54.0.204'
+alias sshver='ssh root@10.54.0.10'
